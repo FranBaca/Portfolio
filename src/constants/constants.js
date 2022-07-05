fbed13681a932ccde786e09d62de69378513f327
FranBaca <franbaca13@gmai.com>
2022-07-05 15:57:27 -0300

    First commit add project

---
+++ src/constants/constants.js
@@ -0,0 +1,45 @@
+export const projects = [
+  {
+    title: 'Henry Hotel',
+    description: "Using React, Node.js, Express & Cloud database, my team and I build a full app for a hotel with check-ins, reservation system, event planning, and admin panel",
+      image: '/images/1.png',
+      tags: ['Cloud', 'Express', 'React', 'Node'],
+    source: 'https://henryhotel.hruiz.com/',
+    visit: 'https://github.com/FranBaca/ecommerce-FT10-G2',
+    id: 0,
+  },
+  {
+    title: 'Countries App',
+    description:"Using React, Node.js, PostgresSQL, Express, i build a full stack app for tracking countries, display information about every country, and plan your vacations",
+    image: '/images/2.png',
+    tags: ['React', 'JavaScript', 'Express', 'Node', 'PostgresSQL'],
+    source: 'https://country-app-eosin.vercel.app/',
+    visit: 'https://github.com/FranBaca/Country-App',
+    id: 1,
+  },
+  {
+    title: 'Rick & Morty App',
+    description: "A simple web app to display information about all Rick & Morty's series characters",
+      image: '/images/3.jpg',
+      tags: ['React', 'JavaScript','Node','API REST'],
+    source: 'https://rick-nm-orty-app-deploy.vercel.app/',
+    visit: 'https://github.com/FranBaca/RickNMortyApp',
+    id: 2,
+  },
+  {
+    title: 'Crypto Tracker',
+    description: "This is a Web App, connected to API REST that tracks all active cryptocurrencies, displays his values, and data charts",
+    image: '/images/4.png',
+    tags: ['React', 'API REST', 'Material-UI'],
+    source: 'https://crypto-react-app-rho.vercel.app/',
+    visit: 'https://github.com/FranBaca/Crypto-React-App',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2018, text: 'Entered university & started learning code', },
+  { year: 2019, text: 'Drop out of university & started the Henry Full stack dev bootcamp', },
+  { year: 2020, text: 'Got my first job as a developer', },
+  { year: 2021, text: 'Got my second job as a developer', },
+];
---
+++ src/constants/data.js
@@ -0,0 +1,21 @@
+export const experience =[
+    
+    
+        {
+            id:1,
+            title:"Zexel Startup",
+            time:"2020-2022",
+            img:"/images/zexel.jpg",
+            description:"Front-End Development (Javascript, React, Figma) Responsible of creating new interfaces MVP Development and V2 version of Web App",
+            link: "https://www.zexel.io/"
+        },
+        {
+            id:2,
+            title:"Agrow Analytics",
+            time:"2021-2022",
+            img:"/images/Agrow.png",
+            description:"Full stack development(Angular, MySQL,) Maintenance of Web App Progressive web app development for mobile",
+            link: "https://www.agrowanalytics.com/"
+        },
+    
+]
